1a8cc63735ad6e4f4dcb787dd5c2d12a92ae4b4f
James Vance <jvance@ictp.it>
2017-06-21 20:46:05 +0200

    Updated tests for subroutines in dmrg.hpp

--- src/test_dmrg.cpp
+++ src/test_dmrg.cpp
@@ -1,5 +1,5 @@
 static char help[] =
-    "Test Code for Kronecker product\n";
+    "Test Code for DMRG\n";
 
 #include <iostream>
 #include <slepceps.h>
@@ -19,10 +19,25 @@ int main(int argc, char **argv)
     MPI_Comm_size(comm, &nprocs);
     MPI_Comm_rank(comm, &rank);
 
-    iDMRG dmrg;
-    dmrg.init();
+    DMRGBlock block;
+    block.init();
 
-    dmrg.destroy();
+    Mat Sz = block.Sz();
+    Mat Sp = block.Sp();
+
+    Mat eye2;
+    MatEyeCreate(comm, eye2, 10);
+
+    Mat H_temp;
+    MatKron(eye2, Sp, H_temp, comm);
+
+    MatPeek(comm,Sz, "Sz");
+    MatPeek(comm,Sp, "Sp");
+    MatPeek(comm,eye2, "eye2");
+    MatPeek(comm,H_temp, "H_temp");
+
+    block.destroy();
+
     SlepcFinalize();
     return ierr;
 }
