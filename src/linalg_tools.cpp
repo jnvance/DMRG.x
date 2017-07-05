85ed4515cfb647e712ec162a5fdee5a6722a104d
James Vance <jvance@ictp.it>
2017-07-05 12:07:41 +0200

    Implemented VecReshapeToLocalMat and VecToMatMultHC with limited capabilities

--- src/linalg_tools.cpp
+++ src/linalg_tools.cpp
@@ -1,7 +1,6 @@
 
 #include "linalg_tools.hpp"
 
-
 #undef __FUNCT__
 #define __FUNCT__ "MatEyeCreate"
 PetscErrorCode MatEyeCreate(MPI_Comm comm, Mat& eye, PetscInt dim)
@@ -99,7 +98,14 @@ PetscErrorCode MatWrite(const MPI_Comm comm, const Mat mat, const char* filename
     PetscViewer writer = nullptr;
     MatAssemblyBegin(mat, MAT_FLUSH_ASSEMBLY);
     MatAssemblyEnd(mat, MAT_FINAL_ASSEMBLY);
-    PetscViewerBinaryOpen(comm, filename, FILE_MODE_WRITE, &writer);
+    if(comm==PETSC_COMM_SELF){
+        char filename_[80]; PetscMPIInt rank; MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
+        sprintf(filename_, "%s.%d", filename, rank);
+        PetscViewerBinaryOpen(comm, filename_, FILE_MODE_WRITE, &writer);
+    }
+    else {
+        PetscViewerBinaryOpen(comm, filename, FILE_MODE_WRITE, &writer);
+    }
     MatView(mat, writer);
     PetscViewerDestroy(&writer);
     writer = nullptr;
@@ -148,7 +154,7 @@ PetscErrorCode VecPeek(const MPI_Comm& comm, const Vec& vec, const char* label)
 /* Reshape m*n vector to m x n array */
 #undef __FUNCT__
 #define __FUNCT__ "VecReshapeToMat"
-PetscErrorCode VecReshapeToMat(const MPI_Comm& comm, const Vec& vec, Mat& mat, const PetscInt M, const PetscInt N)
+PetscErrorCode VecReshapeToMat(const MPI_Comm& comm, const Vec& vec, Mat& mat, const PetscInt M, const PetscInt N, const PetscBool mat_is_local = PETSC_FALSE)
 {
 
     PetscErrorCode  ierr = 0;
@@ -169,9 +175,16 @@ PetscErrorCode VecReshapeToMat(const MPI_Comm& comm, const Vec& vec, Mat& mat, c
     Vec         subvec;
 
     /*
-        TODO: Assign the data pointer beforehand, and dump vector
+        Matrix may be created locally as sequential or globally with MPI
     */
-    MatCreateDense(comm, PETSC_DECIDE, PETSC_DECIDE, M, N, NULL, &mat);
+    if (mat_is_local == PETSC_TRUE)
+    {
+        MatCreateSeqDense(PETSC_COMM_SELF, M, N, NULL, &mat);
+    }
+    else
+    {
+        MatCreateDense(comm, PETSC_DECIDE, PETSC_DECIDE, M, N, NULL, &mat);
+    }
 
     MatGetOwnershipRange(mat, &mat_Istart, &mat_Iend);
 
@@ -183,22 +196,14 @@ PetscErrorCode VecReshapeToMat(const MPI_Comm& comm, const Vec& vec, Mat& mat, c
     PetscMalloc1(subvec_nitems, &vec_idx);
     for (int i = 0; i < subvec_nitems; ++i)
         vec_idx[i] = subvec_Istart + i;
-    ISCreateGeneral(comm, subvec_nitems, vec_idx, PETSC_OWN_POINTER, &vec_is); /* vec_idx is now owned by vec_is */
+    ISCreateGeneral(comm, subvec_nitems, vec_idx, PETSC_OWN_POINTER, &vec_is);
+    /* vec_idx is now owned by vec_is */
 
     VecGetSubVector(vec, vec_is, &subvec);
 
     PetscScalar*    subvec_array;
     VecGetArray(subvec, &subvec_array);
 
-
-    // PetscViewer fd = nullptr;
-    // PetscPrintf(comm, "Sub Vector\n");
-    // ierr = VecView(subvec, fd); CHKERRQ(ierr);
-    // PetscViewerDestroy(&fd);
-
-
-    PetscMPIInt rank;
-    MPI_Comm_rank(comm, &rank);
 
     PetscInt*   col_idx;
     PetscMalloc1(N, &col_idx);
@@ -220,25 +225,94 @@ PetscErrorCode VecReshapeToMat(const MPI_Comm& comm, const Vec& vec, Mat& mat, c
 }
 
 
-/* Reshape m*n vector to m x n array */
+PetscErrorCode VecReshapeToLocalMat(const MPI_Comm& comm, const Vec& vec,
+    Mat& mat, const PetscInt M, const PetscInt N)
+{
+    PetscErrorCode ierr = 0;
+    ierr = VecReshapeToMat(comm, vec, mat, M, N, PETSC_TRUE); CHKERRQ(ierr);
+    return ierr;
+};
+
+
 #undef __FUNCT__
-#define __FUNCT__ "VecToMatMultHermitianTranspose"
-PetscErrorCode VecToMatMultHermitianTranspose(const MPI_Comm& comm, const Vec& vec_r, const Vec& vec_i, Mat& mat, const PetscInt M, const PetscInt N, const PetscBool )
+#define __FUNCT__ "VecToMatMultHC"
+PetscErrorCode VecToMatMultHC(const MPI_Comm& comm, const Vec& vec_r, const Vec& vec_i,
+    Mat& mat, const PetscInt M, const PetscInt N, const PetscBool hc_right = PETSC_TRUE)
 {
 
     PetscErrorCode  ierr = 0;
 
-
-    /* Brute-force implementation */
-    /* Collect entire groundstate vector to all processes */
-
-
-    #if defined(PETSC_USE_COMPLEX)
-
-    #else
+    #ifndef PETSC_USE_COMPLEX
         SETERRQ(comm, 1, "Not implemented for real scalars.");
     #endif
 
+    /*
+        Get the size of vec and determine whether the size of the ouput matrix
+        is compatible with this size, i.e. vec_size == M*N
+     */
+    PetscInt vec_size;
+    ierr = VecGetSize(vec_r, &vec_size); CHKERRQ(ierr);
+    if( M * N != vec_size ) SETERRQ(comm, 1, "Size mismatch");
+
+    /*
+        Collect entire vector into sequential matrices residing in each process
+    */
+    Mat gsv_mat_seq = nullptr;
+    // ierr = VecReshapeToLocalMat(comm, vec_r, gsv_mat_seq, M, N ); CHKERRQ(ierr);
+    ierr = VecReshapeToLocalMat(comm, vec_r, gsv_mat_seq, M, N); CHKERRQ(ierr);
+
+    /*
+        Create the resultant matrix mat with the correct dimensions
+    */
+    PetscInt    mat_dim;
+    if (hc_right == PETSC_TRUE){
+        mat_dim = M;
+    }
+    else {
+        SETERRQ(comm, 1, "Hermitian conjugate on left matrix not yet supported.");
+        mat_dim = N;
+    }
+
+    ierr = MatCreateDense(comm, PETSC_DECIDE, PETSC_DECIDE, mat_dim, mat_dim, NULL, &mat); CHKERRQ(ierr);
+
+    /*
+        Create a matrix object that handles the local portion of mat
+    */
+    Mat mat_local;
+    MatDenseGetLocalMatrix(mat, &mat_local);
+    /*
+        Get ownership info
+    */
+    PetscInt    Istart, Iend, nrows;
+    ierr = MatGetOwnershipRange(mat, &Istart, &Iend); CHKERRQ(ierr);
+    nrows = Iend - Istart;
+    /*
+        Create a copy of the portion of gsv_mat that mimics the local row partition of mat
+    */
+    Mat gsv_mat_loc;
+    ierr = MatCreateSeqDense(PETSC_COMM_SELF, nrows, N, NULL, &gsv_mat_loc); CHKERRQ(ierr);
+    /*
+        Fill gsv_mat_loc with column slices of gsv_mat_seq that belong to the local row partition of mat
+    */
+    PetscScalar *vals_gsv_mat_seq, *vals_gsv_mat_loc;
+    ierr = MatDenseGetArray(gsv_mat_seq, &vals_gsv_mat_seq); CHKERRQ(ierr);
+    ierr = MatDenseGetArray(gsv_mat_loc, &vals_gsv_mat_loc); CHKERRQ(ierr);
+    for (PetscInt Icol = 0; Icol < N; ++Icol)
+    {
+        ierr = PetscMemcpy(&vals_gsv_mat_loc[Icol*nrows],&vals_gsv_mat_seq[Istart+Icol*M], nrows*sizeof(PetscScalar));
+        CHKERRQ(ierr);
+    }
+    ierr = MatDenseRestoreArray(gsv_mat_seq, &vals_gsv_mat_seq); CHKERRQ(ierr);
+    ierr = MatDenseRestoreArray(gsv_mat_loc, &vals_gsv_mat_loc); CHKERRQ(ierr);
+
+    Mat gsv_mat_hc;
+    ierr = MatHermitianTranspose(gsv_mat_seq, MAT_INITIAL_MATRIX, &gsv_mat_hc); CHKERRQ(ierr);
+
+    MatMatMult(gsv_mat_loc,gsv_mat_hc,MAT_REUSE_MATRIX,PETSC_DEFAULT,&mat_local);
+
+    if(gsv_mat_seq) {ierr = MatDestroy(&gsv_mat_seq); CHKERRQ(ierr);}
+    if(gsv_mat_loc) {ierr = MatDestroy(&gsv_mat_loc); CHKERRQ(ierr);}
+    if(gsv_mat_hc ) {ierr = MatDestroy(&gsv_mat_hc ); CHKERRQ(ierr);}
+
     return ierr;
 }
-
--- src/linalg_tools.hpp
+++ src/linalg_tools.hpp
@@ -25,7 +25,32 @@ PetscErrorCode VecWrite(const MPI_Comm& comm, const Vec& vec, const char* filena
 PetscErrorCode VecPeek(const MPI_Comm& comm, const Vec& vec, const char* label);
 
 
-/* Reshape m*n vector to m x n array */
-PetscErrorCode VecReshapeToMat(const MPI_Comm& comm, const Vec& vec, Mat& mat, const PetscInt M, const PetscInt N);
+/**
+ * Reshape m*n vector to m x n array
+ */
+PetscErrorCode VecReshapeToMat(
+    const MPI_Comm& comm, const Vec& vec, Mat& mat,
+    const PetscInt M, const PetscInt N, const PetscBool mat_is_local);
+
+
+/**
+ * Reshape m*n vector to m x n array and store array locally
+ */
+PetscErrorCode VecReshapeToLocalMat(
+    const MPI_Comm& comm, const Vec& vec, Mat& mat, const PetscInt M, const PetscInt N);
+
+
+/**
+ *  Reshapes a vector into a matrix and multiplies the matrix to its own Hermitian transpose
+ *
+ *  This function reshapes the vector vec = vec_r + i*vec_i into matrix A with shape (M,N).
+ *  With transpose_right = PETSC_TRUE, the output is mat = A * A^dag with shape (M,M), where dag is
+ *  the Hermitian conjugate. Otherwise, mat = A^dag * A with shape (N,N)
+ *
+ *  Note: This function is implemented only for complex scalars so that vec_i is ignored.
+ */
+PetscErrorCode VecToMatMultHC(const MPI_Comm& comm, const Vec& vec_r, const Vec& vec_i,
+    Mat& mat, const PetscInt M, const PetscInt N, const PetscBool hc_right);
+
 
 #endif // __LINALG_TOOLS_HPP__
