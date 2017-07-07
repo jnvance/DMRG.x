1532db5d6c1d27c542f07c393836813ecd7225c7
James Vance <jvance@ictp.it>
2017-07-07 16:40:25 +0200

    Expanded documentation for Kronecker Product module

--- src/kron.cpp
+++ src/kron.cpp
@@ -2,9 +2,9 @@
 
 
 #undef __FUNCT__
-#define __FUNCT__ "MatKronScaleAdd"
+#define __FUNCT__ "MatKron"
 PetscErrorCode
-MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm)
+MatKron(const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm)
 {
     PetscErrorCode ierr = 0;
 
@@ -27,98 +27,158 @@ MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const M
     M_C = M_A * M_B;
     N_C = N_A * N_B;
 
-    // // Setup matrix C
-    // // TODO: maybe hardcode some setup options
-    // MatCreate(comm, &C);
-    // MatSetSizes(C, PETSC_DECIDE, PETSC_DECIDE, M_C, N_C);
-    // MatSetFromOptions(C);
-    // MatSetUp(C);
-    // MatZeroEntries(C);
+    // Setup matrix C
+    // TODO: maybe hardcode some setup options
+    MatCreate(comm, &C);
+    MatSetSizes(C, PETSC_DECIDE, PETSC_DECIDE, M_C, N_C);
+    MatSetFromOptions(C);
+    MatSetUp(C);
+    MatZeroEntries(C);
 
-    // Determine whether C has the correct size
+    MatKronAdd(A, B, C, comm);
+
+    return ierr;
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "MatKronAdd"
+PetscErrorCode
+MatKronAdd(const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm)
+{
+    PetscErrorCode ierr = 0;
+
+    ierr = MatKronScaleAdd(1., A, B, C, comm); CHKERRQ(ierr);
+
+    return ierr;
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "MatKronScaleAdd"
+PetscErrorCode
+MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm)
+{
+    PetscErrorCode ierr = 0;
+
+    PetscMPIInt     nprocs, rank;
+    PetscInt        M_A, N_A, M_B, N_B, M_C, N_C;
+    /*
+        Get information from MPI
+    */
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &rank);
+    /*
+        Put input matrices in correct state for submatrix extraction
+    */
+    ierr = MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatAssemblyBegin(B, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(B, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    /*
+        Determine dimensions of C and initialize
+    */
+    MatGetSize(A, &M_A, &N_A);
+    MatGetSize(B, &M_B, &N_B);
+    M_C = M_A * M_B;
+    N_C = N_A * N_B;
+    /*
+        Determine whether C has the correct size
+    */
     PetscInt M_C_input, N_C_input;
     MatGetSize(C, &M_C_input, &N_C_input);
     if( (M_C_input != M_C) || (N_C_input != N_C) ){
         SETERRQ(comm,1,"Incorrect Matrix size");
         PetscPrintf(comm, "Input(%d, %d) != Expected(%d, %d)\n", M_C_input, N_C_input, M_C, N_C);
     }
-
+    /*
+        Create the submatrix for A
+    */
     PetscInt Istart, Iend;
-    MatGetOwnershipRange(C, &Istart, &Iend);
-
-    // Determine required rows from A
     Mat submat_A;
+    /*
+        Determine required rows from A
+    */
+    MatGetOwnershipRange(C, &Istart, &Iend);
     PetscInt Astart, Aend;
-    Astart = Istart/M_B;        // CHECK denominators
-    Aend = 1+(Iend-1)/M_B;      // overestimate Aend
+    Astart = Istart/M_B;
+    Aend = 1+(Iend-1)/M_B;
     PetscInt M_req_A = Aend - Astart;
-
-    // List down indices needed for submatrix A
+    /*
+        List down indices needed for submatrix A
+    */
     PetscInt id_rows_A[M_req_A], id_cols_A[N_A];
     for (PetscInt Irow = Astart; Irow < Aend; ++Irow)
         id_rows_A[Irow-Astart] = Irow;
     for (PetscInt Icol = 0; Icol < N_A; ++Icol)
         id_cols_A[Icol] = Icol;
-
-    // Construct index set
+    /*
+        Construct index set
+    */
     IS isrow_A, iscol_A;
     ISCreateGeneral(comm, M_req_A, id_rows_A, PETSC_COPY_VALUES, &isrow_A);
     ISCreateGeneral(comm, N_A,     id_cols_A, PETSC_COPY_VALUES, &iscol_A);
-
-    // Construct submatrix_A
+    /*
+        Construct submatrix_A and get local indices
+    */
     MatGetSubMatrix(A, isrow_A, iscol_A, MAT_INITIAL_MATRIX, &submat_A);
-
     PetscInt A_sub_start, A_sub_end;
     MatGetOwnershipRange(submat_A, &A_sub_start, &A_sub_end);
-
+    /*
+        Test: Confirm correct ownership
+    */
     #ifdef __PRINT_TESTS__
-    // Test: Confirm correct ownership
-    for(PetscInt Irank = 0; Irank < nprocs; ++Irank){
-        if(Irank==rank)
-            printf("[%2d] A_submat start: %-5d end: %-5d\n", rank, A_sub_start, A_sub_end);
-        MPI_Barrier(comm);
-    }
-    PetscPrintf(comm, "\nRow index set for A\n");
-    ISView(isrow_A, PETSC_VIEWER_STDOUT_WORLD);
+        for(PetscInt Irank = 0; Irank < nprocs; ++Irank){
+            if(Irank==rank)
+                printf("[%2d] A_submat start: %-5d end: %-5d\n", rank, A_sub_start, A_sub_end);
+            MPI_Barrier(comm);
+        }
+        PetscPrintf(comm, "\nRow index set for A\n");
+        ISView(isrow_A, PETSC_VIEWER_STDOUT_WORLD);
     #endif
+    /*
+        Create submatrix for B
 
-    // Get indices for submatrix B, with each process taking all elements
-    // TODO: Reallocate aligned
-    // TODO: Collect elements needed only on processor
-    //       If processor takes no non-zero value in A, do not take values for B
+        Get indices for submatrix B, with each process taking all elements
+        TODO: Reallocate aligned
+        TODO: Collect elements needed only on processor
+              If processor takes no non-zero value in A, do not take values for B
+    */
     Mat submat_B;
     PetscInt id_rows_B[M_B], id_cols_B[N_B];
     for (PetscInt Irow = 0; Irow < M_B; ++Irow)
         id_rows_B[Irow] = Irow;
     for (PetscInt Icol = 0; Icol < N_B; ++Icol)
         id_cols_B[Icol] = Icol;
-
-    // Construct index set
+    /*
+        Construct index set
+    */
     IS isrow_B, iscol_B;
     ISCreateGeneral(comm, M_B, id_rows_B, PETSC_COPY_VALUES, &isrow_B);
     ISCreateGeneral(comm, N_B, id_cols_B, PETSC_COPY_VALUES, &iscol_B);
-
-    // Construct submatrix_B
+    /*
+        Construct submatrix_B and get local indices
+    */
     MatGetSubMatrix(B, isrow_B, iscol_B, MAT_INITIAL_MATRIX, &submat_B);
-
     PetscInt B_sub_start, B_sub_end;
     MatGetOwnershipRange(submat_B, &B_sub_start, &B_sub_end);
-
-    // Map ownership
-    // Input: the row INDEX in the global matrix A/B
-    // Output: the corresponding row index in the locally-owned rows of submatrix A/B
+    /*
+        Map ownership
+        Input: the row INDEX in the global matrix A/B
+        Output: the corresponding row index in the locally-owned rows of submatrix A/B
+    */
     #define ROW_MAP_A(INDEX) ((INDEX) - Astart + A_sub_start)
     #define ROW_MAP_B(INDEX) ((INDEX) + B_sub_start)
-
-
-    // Submatrix constructions offsets the starting column
-    // Input: the corresponding column index in the locally-owned submatrix A/B
-    // Output: the column INDEX in the global matrix A/B
+    /*
+        Submatrix constructions offsets the starting column
+        Input: the corresponding column index in the locally-owned submatrix A/B
+        Output: the column INDEX in the global matrix A/B
+    */
     #define COL_MAP_A(INDEX) ((INDEX) - N_A * (nprocs - 1) )
     #define COL_MAP_B(INDEX) ((INDEX) - N_B * (nprocs - 1) )
-
-
-    // Determine non-zero indices of submatrices
+    /*
+        Determine non-zero indices of submatrices
+    */
     const PetscInt*     cols_A;
     const PetscScalar*  vals_A;
     const PetscInt*     cols_B;
@@ -126,22 +186,24 @@ MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const M
     PetscInt            ncols_A, ncols_B;
     PetscInt            Arow, Brow;
     PetscInt            ncols_C;
-
-    // IMPLEMENTATION OPTIONS:
-    // Load A and B one row at a time (check)
-    // Pre-allocate an array for calculating a row of C using
-    //     the maximum possible number of columns scanned through all A and B
-    // Load B all at once and destroy submatrix
-    //     Find a way to get and set values using the entire submatrix B
-
+    /*
+        IMPLEMENTATION OPTIONS:
+        Load A and B one row at a time (check)
+        Pre-allocate an array for calculating a row of C using
+            the maximum possible number of columns scanned through all A and B
+        Load B all at once and destroy submatrix
+            Find a way to get and set values using the entire submatrix B
+    */
     // #define __SEQ_ORDER__
     #ifdef __SEQ_ORDER__
+
     for(PetscInt Irank = 0; Irank < nprocs; ++Irank){
         if(Irank==rank)
-    #endif
 
+    #endif
     // #define __NO_KRON__
     #ifndef __NO_KRON__
+
             for (PetscInt Irow = Istart; Irow < Iend; ++Irow)
             {
                 Arow = Irow/M_B;
@@ -156,9 +218,10 @@ MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const M
                 #endif
 
                 ncols_C = ncols_A * ncols_B;
-
-                // This malloc might be costly, try to estimate the max number of nonzeros
-                // of the product matrix and pre-allocate outside the loop
+                /*
+                    TODO: This malloc might be costly, try to estimate the max number of nonzeros
+                    of the product matrix and pre-allocate outside the loop
+                */
                 PetscInt*       cols_C = new PetscInt[ncols_C];
                 PetscScalar*    vals_C = new PetscScalar[ncols_C];
 
@@ -206,7 +269,9 @@ MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const M
     #undef ROW_MAP_B
 
 
-    /*Write submatrices to file*/
+    /*
+        Write submatrices to file
+    */
     // #define __KRON_WRITE_SUBMAT__
     #ifdef __KRON_WRITE_SUBMAT__
         PetscViewer writer = nullptr;
@@ -228,56 +293,5 @@ MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const M
     MatDestroy(&submat_A);
     MatDestroy(&submat_B);
     // if(submat_C) MatDestroy(&submat_C);
-    return ierr;
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "MatKronAdd"
-PetscErrorCode
-MatKronAdd(const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm)
-{
-    PetscErrorCode ierr = 0;
-
-    ierr = MatKronScaleAdd(1., A, B, C, comm); CHKERRQ(ierr);
-
-    return ierr;
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "MatKronScaleAdd"
-PetscErrorCode
-MatKron(const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm)
-{
-    PetscErrorCode ierr = 0;
-
-    PetscMPIInt     nprocs, rank;           // MPI comm variables
-    PetscInt        M_A, N_A, M_B, N_B, M_C, N_C;
-
-    // Get information from MPI
-    MPI_Comm_size(comm, &nprocs);
-    MPI_Comm_rank(comm, &rank);
-
-    // Put input matrices in correct state for submatrix extraction
-    ierr = MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-    ierr = MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-    ierr = MatAssemblyBegin(B, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-    ierr = MatAssemblyEnd(B, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-
-    // Determine dimensions of C and initialize
-    MatGetSize(A, &M_A, &N_A);
-    MatGetSize(B, &M_B, &N_B);
-    M_C = M_A * M_B;
-    N_C = N_A * N_B;
-
-    // Setup matrix C
-    // TODO: maybe hardcode some setup options
-    MatCreate(comm, &C);
-    MatSetSizes(C, PETSC_DECIDE, PETSC_DECIDE, M_C, N_C);
-    MatSetFromOptions(C);
-    MatSetUp(C);
-    MatZeroEntries(C);
-
-    MatKronAdd(A, B, C, comm);
-
     return ierr;
 }
--- src/kron.hpp
+++ src/kron.hpp
@@ -1,39 +1,83 @@
 #ifndef __KRON_HPP__
 #define __KRON_HPP__
 
-/** @defgroup Kronecker
- *  @brief Implementation of the kronecker product with distributed sparse matrices in PETSc and SLEPc (v3.7)
- */
-
 #include <slepceps.h>
 #include <stdlib.h>
 
+
 /**
-    In this implementation of the kronecker product $ C = A \otimes B $ we
-    require that each process have a local copy of the parallel matrix B. This
-    is achieved by getting the submatrices of A and C
+    @defgroup KroneckerProduct
+    @brief    Implementation of the Kronecker product with distributed sparse matrices
+
+    Given an \f$m_A \times n_A\f$ matrix \f$\mathsf{A}\f$ and \f$m_B \times n_B\f$ matrix \f$\mathsf{B}\f$,
+    their Kronecker product \f$ \mathsf{C} = \mathsf{A} \otimes \mathsf{B} \f$ is an \f$(m_A m_B) \times (n_A n_B)\f$ matrix with elements
+    \f[
+         c_{\alpha\beta} = a_{ij} b_{kl}
+    \f]
+    where
+    \f{eqnarray*}{
+        \alpha &\equiv& m_B \cdot i + k \\
+        \beta  &\equiv& n_B \cdot j + l
+    \f}
+
+    In this implementation, we require that each process have a local copy of the parallel matrix B.
+    This is achieved by obtaining \f$ \mathsf{B} \f$ on each process using a submatrix with [MatGetSubMatrix](http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Mat/MatGetSubMatrix.html).
 
     TODO:
-        * Implement MatKronAdd $ C += A \otimes B $
-        * Implement MatKronScaleAdd $ C += a * A \otimes B $
-        * Implement overloading for when one or more arguments is the identity matrix
-        * Implement overloading for when one or more arguments is the Sz, Sp, or Sm
-        * Reduce communication.
-        * Write a test suite to check whether this works for small and
-            large matrices
-        * Test for the case when A is small and B is large.
-        * Check if this works for sparse x dense (m~10000s)
-        * Check if it works well with other PetscScalar datatypes (complex/real)
+        - Implement overloading for when one or more arguments is the identity matrix
+        - Implement overloading for when one or more arguments is the Sz, Sp, or Sm
+        - Reduce communication.
+        - Write a test suite to check whether this works for small and large matrices
+        - Test for the case when A is small and B is large.
+        - Check if this works for sparse x dense (m~10000s)
+        - Check if it works well with other PetscScalar datatypes (complex/real)
+
+    #### Reference:
+    Weisstein, Eric W. "Kronecker Product." From MathWorld--A Wolfram Web Resource.
+    [http://mathworld.wolfram.com/KroneckerProduct.html](http://mathworld.wolfram.com/KroneckerProduct.html)
+
  */
 
 
-PetscErrorCode MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm);
+/**
+    \addtogroup KroneckerProduct
+    @{
+ */
 
 
-PetscErrorCode MatKronAdd(const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm);
-
-
+/**
+    Implements the Kronecker product between matrices A and B and puts the result in a new matrix C.
+    This function performs
+    \f[
+        \mathsf{C} = \mathsf{A} \otimes \mathsf{B}.
+    \f]
+    The resultant matrix C is created inside this function so the input must be uninitialized.
+ */
 PetscErrorCode MatKron(const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm);
 
 
+/**
+    Implements the Kronecker product between matrices A and B and adds the result to matrix C.
+    This function performs
+    \f[
+        \mathsf{C} = \mathsf{C} + \mathsf{A} \otimes \mathsf{B}.
+    \f]
+    The resultant matrix C must already be initialized and have the correct dimensions.
+ */
+PetscErrorCode MatKronAdd(const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm);
+
+
+/**
+    Implements the Kronecker product between matrices `A` and `B` multiplied by a scale factor `a`
+    and adds the result to matrix `C`.
+    This function performs
+    \f[
+        \mathsf{C} = \mathsf{C} + a \cdot (\mathsf{A} \otimes \mathsf{B}).
+    \f]
+    The resultant matrix C must already be initialized and have the correct dimensions.
+ */
+PetscErrorCode MatKronScaleAdd(const PetscScalar a, const Mat& A, const Mat& B, Mat& C, const MPI_Comm& comm);
+
+
+/** @} */
 #endif // __KRON_HPP__
