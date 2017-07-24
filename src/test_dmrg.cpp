97386b34bbca8c01d5c8961a5b6331a6e3de7c15
James Vance <jvance@ictp.it>
2017-07-24 17:16:18 +0200

    Use CHKERRQ macro more

--- src/test_dmrg.cpp
+++ src/test_dmrg.cpp
@@ -38,14 +38,14 @@ int main(int argc, char **argv)
     iDMRG_Heisenberg heis;
     heis.init(comm, nsites, mstates);
 
-    PetscPrintf(comm,   "\n"
+    ierr = PetscPrintf(comm,   "\n"
                         "iDMRG of the 1D Heisenberg model\n"
                         "Target number of sites  : %-10d\n"
-                        "Number of states to keep: %-10d\n\n", nsites, mstates);
+                        "Number of states to keep: %-10d\n\n", nsites, mstates); CHKERRQ(ierr);
 
     ierr = PetscPrintf(PETSC_COMM_WORLD,
             "   iter     nsites   gs energy   gs energy /site   rel error   ||Ax-kx||/||kx||\n"
-            "  -------- -------- ----------- ----------------- ----------- ------------------\n");CHKERRQ(ierr);
+            "  -------- -------- ----------- ----------------- ----------- ------------------\n"); CHKERRQ(ierr);
     PetscReal gse_r, gse_i, error;
 
     double gse_site_theor =  -0.4431471805599;
@@ -55,13 +55,13 @@ int main(int argc, char **argv)
     {
         PRINT_EMPTY_LINE;
 
-        heis.BuildBlockLeft();
-        heis.BuildBlockRight();
+        ierr = heis.BuildBlockLeft(); CHKERRQ(ierr);
+        ierr = heis.BuildBlockRight(); CHKERRQ(ierr);
 
         if (heis.TotalBasisSize() >= heis.mstates()*heis.mstates())
         {
-            heis.BuildSuperBlock();
-            heis.SolveGroundState(gse_r, gse_i, error);
+            ierr = heis.BuildSuperBlock(); CHKERRQ(ierr);
+            ierr = heis.SolveGroundState(gse_r, gse_i, error); CHKERRQ(ierr);
             superblocklength = heis.LengthBlockLeft() + heis.LengthBlockRight();
 
             PRINT_EMPTY_LINE;
@@ -69,7 +69,7 @@ int main(int argc, char **argv)
             if (gse_i!=0.0) {
                 // TODO: Implement error printing for complex values
                 SETERRQ(comm,1,"Not implemented for complex ground state energy.\n");
-                ierr = PetscPrintf(PETSC_COMM_WORLD," %6d    %9f%+9fi %12g\n", superblocklength, (double)gse_r/((double)(superblocklength)), (double)gse_i/((double)(superblocklength)),(double)error);CHKERRQ(ierr);
+                ierr = PetscPrintf(PETSC_COMM_WORLD," %6d    %9f%+9fi %12g\n", superblocklength, (double)gse_r/((double)(superblocklength)), (double)gse_i/((double)(superblocklength)),(double)error); CHKERRQ(ierr);
             } else {
                 double gse_site  = (double)gse_r/((double)(superblocklength));
                 double error_rel = (gse_site - gse_site_theor) / gse_site_theor;
@@ -78,9 +78,9 @@ int main(int argc, char **argv)
 
             PRINT_EMPTY_LINE;
 
-            heis.BuildReducedDensityMatrices();
-            heis.GetRotationMatrices();
-            heis.TruncateOperators();
+            ierr = heis.BuildReducedDensityMatrices(); CHKERRQ(ierr);
+            ierr = heis.GetRotationMatrices(); CHKERRQ(ierr);
+            ierr = heis.TruncateOperators(); CHKERRQ(ierr);
         } else {
             #if defined(__PRINT_SIZES) || defined(__PRINT_TRUNCATION_ERROR)
                 PetscPrintf(PETSC_COMM_WORLD,"   %6d\n", heis.iter());
@@ -90,7 +90,7 @@ int main(int argc, char **argv)
         heis.iter()++;
     }
 
-    heis.destroy();
+    ierr = heis.destroy(); CHKERRQ(ierr);
 
     SlepcFinalize();
     return ierr;
