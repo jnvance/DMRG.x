bd09f86267d9936be2c1ec6f208dffa8d49aa31e
James Vance <jvance@ictp.it>
2017-09-13 18:31:16 +0200

    Code cleanup and improved printout

--- src/test_dmrg.cpp
+++ src/test_dmrg.cpp
@@ -5,13 +5,6 @@ static char help[] =
 #include "idmrg_1d_heisenberg.hpp"
 
 
-#if defined(__PRINT_SIZES) || defined(__PRINT_TRUNCATION_ERROR)
-    #define PRINT_EMPTY_LINE PetscPrintf(comm,"\n");
-#else
-    #define PRINT_EMPTY_LINE
-#endif
-
-
 #undef __FUNCT__
 #define __FUNCT__ "main"
 int main(int argc, char **argv)
@@ -33,6 +26,7 @@ int main(int argc, char **argv)
     PetscInt mstates = 15;
     PetscScalar J  = 1.0;
     PetscScalar Jz = 1.0;
+    PetscScalar target_Sz = 0.0;
     PetscBool do_target_Sz = PETSC_FALSE;
     PetscBool do_save_operators = PETSC_FALSE;
 
@@ -43,15 +37,10 @@ int main(int argc, char **argv)
     ierr = PetscOptionsGetBool(NULL,NULL,"-do_target_Sz",&do_target_Sz,NULL); CHKERRQ(ierr);
     ierr = PetscOptionsGetBool(NULL,NULL,"-do_save_operators",&do_save_operators,NULL); CHKERRQ(ierr);
 
-
-    // PetscPrintf(comm,"%s\n", do_target_Sz ? "TRUE" : "FALSE");
-
     iDMRG_Heisenberg heis;
-
     heis.init(comm, nsites, mstates);
-
     heis.SetParameters(J, Jz);
-    heis.SetTargetSz(0.0, do_target_Sz);
+    heis.SetTargetSz(target_Sz, do_target_Sz);
 
     /*
         Timings
@@ -73,8 +62,15 @@ int main(int argc, char **argv)
                         "PetscScalar type        : %-s\n"
                         "Target number of sites  : %-d\n"
                         "Number of states to keep: %-d\n"
-                        "Number of MPI processes : %-d\n\n",
-                        J, Jz, scalar_type, nsites, mstates, nprocs); CHKERRQ(ierr);
+                        "Number of MPI processes : %-d\n"
+                        "Do target magnetization : %-s\n",
+                        J, Jz, scalar_type, nsites, mstates, nprocs,
+                        do_target_Sz==PETSC_TRUE ? "yes" : "no" ); CHKERRQ(ierr);
+
+    if(do_target_Sz==PETSC_TRUE) ierr = PetscPrintf(comm,
+                        "Target magnetization    : %-f\n", target_Sz);
+
+    ierr = PetscPrintf(comm,"\n");
 
     ierr = PetscPrintf(PETSC_COMM_WORLD,
             "   iter     nsites   gs energy   gs energy /site   rel error   ||Ax-kx||/||kx||\n"
@@ -108,7 +104,6 @@ int main(int argc, char **argv)
         /*
                 Printout data on ground state energy and wavevector
         */
-        PRINT_EMPTY_LINE;
         superblocklength = heis.LengthBlockLeft() + heis.LengthBlockRight();
         if (gse_i!=0.0) {
             /*
@@ -128,7 +123,6 @@ int main(int argc, char **argv)
                 heis.iter(), superblocklength, (double)gse_r, gse_site,
                 error_rel, (double)(error)); CHKERRQ(ierr);
         }
-        PRINT_EMPTY_LINE;
         /*
             From the ground state wavevector get the reduced density matrices of the left
             and right blocks, and construct the rectangular rotation matrices to perform the
@@ -138,97 +132,9 @@ int main(int argc, char **argv)
         ierr = heis.GetRotationMatrices(); CHKERRQ(ierr);
         ierr = heis.TruncateOperators(); CHKERRQ(ierr);
 
-        // PetscPrintf(PETSC_COMM_WORLD,"   %6d\n", heis.iter());
-
         heis.iter()++;
     }
 
-
-#if 0
-    while(heis.TotalLength() < heis.TargetLength() && heis.iter() < heis.TargetLength())
-    {
-        PRINT_EMPTY_LINE;
-        #ifdef __TESTING
-        ierr = heis.MatSaveOperators(); CHKERRQ(ierr);
-        #endif
-        /*
-            Grow the left and right blocks by adding one site in the junction
-         */
-        ierr = heis.BuildBlockLeft(); CHKERRQ(ierr);
-        ierr = heis.BuildBlockRight(); CHKERRQ(ierr);
-        /*
-            Save operator states and increment
-         */
-        #ifdef __TESTING
-        ierr = heis.BuildSuperBlock(); CHKERRQ(ierr);
-        ierr = heis.MatSaveOperators(); CHKERRQ(ierr);
-        ierr = heis.SolveGroundState(gse_r, gse_i, error); CHKERRQ(ierr);
-        #endif
-        /*
-            Check whether the basis set already need truncation
-         */
-        #ifndef __TESTING
-        if (heis.TotalBasisSize() > heis.mstates()*heis.mstates())
-        {
-            /*
-                The superblock Hamiltonian is constructed and the ground state is solved
-             */
-            // #ifndef __TESTING
-
-            ierr = heis.BuildSuperBlock(); CHKERRQ(ierr);
-            ierr = heis.SolveGroundState(gse_r, gse_i, error); CHKERRQ(ierr);
-
-        #endif
-            /*
-                Printout data on ground state energy and wavevector
-             */
-            PRINT_EMPTY_LINE;
-            superblocklength = heis.LengthBlockLeft() + heis.LengthBlockRight();
-            if (gse_i!=0.0) {
-                /*
-                    TODO: Implement error printing for complex values
-                 */
-                SETERRQ(comm,1,"Not implemented for complex ground state energy.\n");
-                // ierr = PetscPrintf(PETSC_COMM_WORLD," %6d    %9f%+9fi %12g\n",
-                //     superblocklength, (double)gse_r/((double)(superblocklength)),
-                //     (double)gse_i/((double)(superblocklength)),(double)error); CHKERRQ(ierr);
-            } else {
-                double gse_site  = (double)gse_r/((double)(superblocklength));
-                double error_rel = (gse_site - gse_site_theor) / gse_site_theor;
-                ierr = PetscPrintf(PETSC_COMM_WORLD,"   %6d   %6d%12f    %12f     %9f    %12g\n",
-                    heis.iter(), superblocklength, (double)gse_r, gse_site,
-                    error_rel, (double)(error)); CHKERRQ(ierr);
-                ierr = PetscFPrintf(PETSC_COMM_WORLD, fp,"   %6d   %6d    %.20g    %.20g    %.20g    %.20g\n",
-                    heis.iter(), superblocklength, (double)gse_r, gse_site,
-                    error_rel, (double)(error)); CHKERRQ(ierr);
-            }
-            PRINT_EMPTY_LINE;
-
-        #ifdef __TESTING
-        if (heis.TotalBasisSize() > heis.mstates()*heis.mstates())
-        {
-        #endif
-            /*
-                From the ground state wavevector get the reduced density matrices of the left
-                and right blocks, and construct the rectangular rotation matrices to perform the
-                truncation of site and block operators.
-             */
-            ierr = heis.BuildReducedDensityMatrices(); CHKERRQ(ierr);
-            ierr = heis.GetRotationMatrices(); CHKERRQ(ierr);
-            ierr = heis.TruncateOperators(); CHKERRQ(ierr);
-            #ifdef __TESTING
-                ierr = PetscPrintf(PETSC_COMM_WORLD,"%9s>> Truncation \n"," "); CHKERRQ(ierr);
-            #endif
-        } else {
-            #ifndef __TESTING
-                #if defined(__PRINT_SIZES) || defined(__PRINT_TRUNCATION_ERROR)
-                    PetscPrintf(PETSC_COMM_WORLD,"   %6d\n", heis.iter());
-                #endif
-            #endif
-        }
-        heis.iter()++;
-    }
-#endif
 
     ierr = PetscTime(&total_time); CHKERRQ(ierr); \
     total_time = total_time - total_time0; \
