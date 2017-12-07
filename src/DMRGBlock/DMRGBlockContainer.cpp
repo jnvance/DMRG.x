48a259f7d0d7be64fe4356ba2c3471346f08f2e0
James Vance <jvance@ictp.it>
2017-12-07 15:18:15 +0100

    Better documentation and added SingleDMRGStep

--- src/DMRGBlock/DMRGBlockContainer.cpp
+++ src/DMRGBlock/DMRGBlockContainer.cpp
@@ -5,36 +5,39 @@ PetscErrorCode HeisenbergSpinOneHalfLadder::Initialize()
 {
     PetscErrorCode ierr = 0;
 
-    /* Initialize attributes */
+    /*  Initialize attributes  */
     ierr = MPI_Comm_rank(mpi_comm, &mpi_rank); CHKERRQ(ierr);
     ierr = MPI_Comm_size(mpi_comm, &mpi_size); CHKERRQ(ierr);
     ierr = PetscOptionsGetBool(NULL,NULL,"-verbose",&verbose,NULL); CHKERRQ(ierr);
 
-    /* Get couplings and geometry from command line options */
+    /*  Get couplings and information on geometry from command line options  */
     ierr = PetscOptionsGetReal(NULL,NULL,"-J1",&J1,NULL); CHKERRQ(ierr);
     ierr = PetscOptionsGetReal(NULL,NULL,"-J2",&J2,NULL); CHKERRQ(ierr);
     ierr = PetscOptionsGetInt(NULL,NULL,"-Lx",&Lx,NULL); CHKERRQ(ierr);
     ierr = PetscOptionsGetInt(NULL,NULL,"-Ly",&Ly,NULL); CHKERRQ(ierr);
+    ierr = PetscOptionsGetInt(NULL,NULL,"-mstates",&mstates,NULL); CHKERRQ(ierr);
 
     num_sites = Lx * Ly;
     num_blocks = num_sites - 1;
 
-    /* Initialize a static block of single-site operators */
+    /*  Require that the total number of sites be even */
+    if(num_sites % 2) SETERRQ3(mpi_comm, 1, "Total number of sites must be even. "
+        "Given: Lx=%d, Ly=%d, Lx*Ly=%d", Lx, Ly, num_sites);
+
+    /*  Initialize a static block of single-site operators  */
     ierr = SingleSite.Initialize(mpi_comm, 1, PETSC_DEFAULT); CHKERRQ(ierr);
 
-    /* Allocate the system array of blocks with single sites */
+    /*  Allocate the array of system blocks */
     SysBlocks = new Block_SpinOneHalf[num_blocks];
 
-    /* Initialize the first system block with one site */
+    /*  Initialize the first system block with one site  */
     ierr = SysBlocks[0].Initialize(mpi_comm, 1, PETSC_DEFAULT); CHKERRQ(ierr);
     ++SysBlocks_num_init;
-    ierr = SysBlocks[1].Initialize(mpi_comm, 1, PETSC_DEFAULT); CHKERRQ(ierr);
-    ++SysBlocks_num_init;
 
-    /* Initialize the environment block with one site */
+    /*  Initialize the environment block with one site  */
     ierr = EnvBlock.Initialize(mpi_comm, 1, PETSC_DEFAULT); CHKERRQ(ierr);
 
-    /* Print some info */
+    /*  Print some info */
     if(verbose)
     {
         ierr = PetscPrintf(mpi_comm,
@@ -45,8 +48,10 @@ PetscErrorCode HeisenbergSpinOneHalfLadder::Initialize()
             "      J2 = %f\n"
             "#   Geometry:\n"
             "      Lx = %d\n"
-            "      Ly = %d\n",
-            J1, J2, Lx, Ly); CHKERRQ(ierr);
+            "      Ly = %d\n"
+            "#   DMRG:\n"
+            "      mstates = %d\n",
+            J1, J2, Lx, Ly, mstates); CHKERRQ(ierr);
     }
 
     return ierr;
@@ -59,15 +64,14 @@ PetscErrorCode HeisenbergSpinOneHalfLadder::Destroy()
 
     ierr = SingleSite.Destroy(); CHKERRQ(ierr);
 
-    /* Deallocate system blocks */
-
+    /*  Deallocate system blocks  */
     for(PetscInt iblock = 0; iblock < SysBlocks_num_init; ++iblock){
         ierr = SysBlocks[iblock].Destroy(); CHKERRQ(ierr);
     }
-
     delete [] SysBlocks;
     SysBlocks = nullptr;
 
+    /*  Deallocate environment block */
     ierr = EnvBlock.Destroy(); CHKERRQ(ierr);
 
     if(verbose){
@@ -77,3 +81,15 @@ PetscErrorCode HeisenbergSpinOneHalfLadder::Destroy()
     return ierr;
 }
 
+PetscErrorCode HeisenbergSpinOneHalfLadder::SingleDMRGStep(
+    const Block_SpinOneHalf& Sys,
+    const Block_SpinOneHalf& Env
+    )
+{
+    PetscErrorCode ierr = 0;
+
+    // if(&Sys==&Env) // object comparison by pointers
+
+
+    return ierr;
+}
--- src/DMRGBlock/DMRGBlockContainer.hpp
+++ src/DMRGBlock/DMRGBlockContainer.hpp
@@ -15,7 +15,7 @@ class HeisenbergSpinOneHalfLadder
 
 private:
 
-    /*------ Backend ------*/
+    /*------ MPI and Misc ------*/
 
     /** MPI Communicator */
     MPI_Comm    mpi_comm = PETSC_COMM_WORLD;
@@ -37,6 +37,12 @@ private:
     /** Coupling strength for next-nearest-neighbor interactions */
     PetscScalar J2 = 1.0;
 
+    /*------ DMRG Attributes ------*/
+
+    /** Target maximum number of states after truncation */
+    PetscInt    mstates = 20;
+
+
     /*------ Geometry ------*/
 
     /** Length along (growing) longitudinal direction */
@@ -48,12 +54,15 @@ private:
     /** Total number of sites */
     PetscInt    num_sites;
 
+public:
+
     /*------ Blocks ------*/
 
     /** Number of system blocks to store, usually Lx*Ly-1 */
     PetscInt    num_blocks;
 
-    /** Array of blocks */
+    /** Array of system blocks each of which will be kept
+        all throughout the simulation */
     Block_SpinOneHalf *SysBlocks;
 
     /** Number of initialized blocks in SysBlocks */
@@ -62,267 +71,22 @@ private:
     /** Environment block to be used only during warmup */
     Block_SpinOneHalf EnvBlock;
 
-public:
-
     /** Static block containing single site operators for reference */
     Block_SpinOneHalf SingleSite;
 
-
-
+    /** Initializes the container object */
     PetscErrorCode Initialize();
 
+    /** Performs one single DMRG warmup step */
+    PetscErrorCode SingleDMRGStep(
+        const Block_SpinOneHalf& Sys,
+        const Block_SpinOneHalf& Env
+        );
+
+    /** Destroys the container object */
     PetscErrorCode Destroy();
 
 };
 
-
-
-
-
-
-
-
-
-
-
-#define false 0
-#if false
-typedef enum
-{
-    BlockSide_Left=0,
-    BlockSide_Right=1
-} BlockSide_t;
-
-
-template <class SiteType>
-class DMRGBlockContainer
-{
-
-private:
-
-    MPI_Comm comm;
-    PetscMPIInt rank;
-    PetscBool verbose = PETSC_FALSE;
-    BlockSide_t block_side;
-
-    PetscInt nsites_target;
-    PetscInt nsites = 0;
-    PetscInt active_site_idx;
-    PetscInt block_dim_exact;
-
-    SiteType single_site;
-    std::vector<SiteType> sites_list;
-
-public:
-
-    PetscErrorCode Initialize(
-        const MPI_Comm& comm_in,
-        const PetscInt& nsites_target_in,
-        const BlockSide_t& block_side_in);
-    PetscErrorCode Destroy();
-    PetscErrorCode AdjustSiteOps(PetscInt isite);
-    PetscErrorCode AddSite();
-    PetscErrorCode CreateSm();
-    PetscErrorCode DestroySm();
-    PetscErrorCode CheckOperators();
-
-    MPI_Comm Comm() const { return comm; }
-    PetscInt Length() const { return nsites; }
-    PetscInt TargetLength() const { return nsites_target; }
-    BlockSide_t BlockSide() const { return block_side; }
-    PetscInt ActiveSiteIdx() const { return active_site_idx; }
-
-    SiteType& ActiveSite()
-    {
-        return sites_list[active_site_idx];
-    }
-
-    SiteType& Site(PetscInt isite)
-    {
-        return sites_list[isite];
-    }
-
-    SiteType& SingleSite() { return single_site; }
-
-    /************************************************************
-     * Members that depend on magnetization conservation
-     ************************************************************/
-
-    /** Keeps track of the Sz sectors */
-    std::vector<PetscScalar> basis_sector_array;
-
-    /** Keeps track of the basis in each Sz sector */
-    std::unordered_map<PetscScalar,std::vector<PetscInt>> basis_by_sector;
-
-    /** Keeps track of the basis in each Sz sector */
-    std::unordered_map<PetscScalar, Mat> rho_block_dict;
-
-};
-
-
-template <class SiteType>
-PetscErrorCode DMRGBlockContainer<SiteType>::Initialize(
-        const MPI_Comm& comm_in,
-        const PetscInt& nsites_target_in,
-        const BlockSide_t& block_side_in)
-{
-    PetscErrorCode ierr = 0;
-
-    comm = comm_in;
-    nsites_target = nsites_target_in;
-    block_side = block_side_in;
-    MPI_Comm_rank(comm, &rank);
-
-    sites_list.resize(nsites_target);
-
-    /* Initialize single site template */
-    ierr = single_site.Initialize(comm); CHKERRQ(ierr);
-    ierr = single_site.CreateSm(); CHKERRQ(ierr);
-    basis_sector_array = single_site.single_site_sectors;
-
-    /* Initialize exact block dimension */
-    block_dim_exact = single_site.LocDim();
-
-    /* Check whether to do verbose logging */
-    ierr = PetscOptionsGetBool(NULL,NULL,"-verbose",&verbose,NULL); CHKERRQ(ierr);
-
-    /* Initialize a block of one site */
-    ierr = sites_list[nsites].Initialize(comm); CHKERRQ(ierr);
-    active_site_idx = 0;
-    nsites = 1;
-
-    if(verbose && !rank) printf("> block::%s\n",__FUNCTION__);
-    if(verbose && !rank) printf("> block::length %d\n", Length());
-
-    ierr = CheckOperators(); CHKERRQ(ierr);
-    return ierr;
-}
-
-
-template <class SiteType>
-PetscErrorCode DMRGBlockContainer<SiteType>::Destroy()
-{
-    PetscErrorCode ierr = 0;
-
-    ierr = single_site.DestroySm(); CHKERRQ(ierr);
-    ierr = single_site.Destroy(); CHKERRQ(ierr);
-    for (size_t isite = 0; isite < nsites; ++isite)
-    {
-        if (sites_list[isite].Initialized())
-        {
-            ierr = sites_list[isite].Destroy(); CHKERRQ(ierr);
-        }
-    }
-
-    return ierr;
-}
-
-
-template <class SiteType>
-PetscErrorCode DMRGBlockContainer<SiteType>::AddSite()
-{
-    PetscErrorCode ierr = 0;
-
-    /*  Check that the number of sites is within the target */
-    if (nsites+1 > nsites_target)
-        SETERRQ1(comm, 1, "Cannot add more sites. Block length will exceed the target (%d)", nsites_target);
-
-    ierr = CheckOperators(); CHKERRQ(ierr);
-
-    /*  Initialize a new site  */
-    ierr = sites_list[nsites].Initialize(comm); CHKERRQ(ierr);
-
-    active_site_idx = nsites;
-    ++nsites;
-
-    if(verbose && !rank) printf("> block::%s\n",__FUNCTION__);
-    return ierr;
-}
-
-
-template <class SiteType>
-PetscErrorCode DMRGBlockContainer<SiteType>::AdjustSiteOps(PetscInt isite)
-{
-    PetscErrorCode ierr = 0;
-
-    /*
-        Change its single operator representation by appending an identity representing the combined
-        Hilbert space of previously added sites ...
-     */
-    if(verbose && !rank) printf("> isite %d\n", isite);
-
-    // ierr = BuildBlockHamiltonian(&this, isite); CHKERRQ(ierr);
-    // ierr = PetscPrintf(this->Comm(), "H_adds: %p\n", this->Site(isite-1).H());
-    // if(!sites_list[isite].H()) SETERRQ(comm, 1, "Matrix is null.");
-    // ierr = MatPeek(sites_list[isite].H(),"sites_list"); CHKERRQ(ierr);
-
-    /* TODO: Think about generalizing this */
-    PetscInt block_dim = sites_list[isite-1].MatOpDim();
-    if(block_side==BlockSide_Left){
-        /* ... on the left side of the new operator on the left block */
-        ierr = sites_list[isite].EyeKronOp(block_dim); CHKERRQ(ierr);
-    } else {
-        /* ... on the right side of the new operator on the right block */
-        ierr = sites_list[isite].OpKronEye(block_dim); CHKERRQ(ierr);
-    }
-
-    if(verbose && !rank) printf("> block::%s\n",__FUNCTION__);
-    return ierr;
-}
-
-
-template <class SiteType>
-PetscErrorCode DMRGBlockContainer<SiteType>::CreateSm()
-{
-    PetscErrorCode ierr = 0;
-
-    for (size_t isite = 0; isite < nsites; ++isite)
-    {
-        if (sites_list[isite].Initialized())
-        {
-            ierr = sites_list[isite].CreateSm(); CHKERRQ(ierr);
-        }
-    }
-
-    return ierr;
-}
-
-
-template <class SiteType>
-PetscErrorCode DMRGBlockContainer<SiteType>::DestroySm()
-{
-    PetscErrorCode ierr = 0;
-
-    for (size_t isite = 0; isite < nsites; ++isite)
-    {
-        if (sites_list[isite].Initialized())
-        {
-            ierr = sites_list[isite].DestroySm(); CHKERRQ(ierr);
-        }
-    }
-
-    return ierr;
-}
-
-
-template <class SiteType>
-PetscErrorCode DMRGBlockContainer<SiteType>::CheckOperators()
-{
-    PetscErrorCode ierr = 0;
-
-    /* TODO: Check only active and initialized sites */
-    // block_dim_exact = Site(0).MatOpDim();
-    // ierr = Site(0).CheckOperators(); CHKERRQ(ierr);
-    // for (size_t isite = 1; isite < nsites; ++isite)
-    // {
-    //     ierr = Site(isite).CheckOperators(); CHKERRQ(ierr);
-    //     PetscInt block_dim_exact_site = Site(isite).MatOpDim();
-    //     if(block_dim_exact_site!=block_dim_exact) SETERRQ3(comm, 1, "Site %d has different operator dimension. "
-    //         "Expected %d. Got %d.", isite, block_dim_exact, block_dim_exact_site);
-    // }
-
-    return ierr;
-}
-#endif
 
 #endif // __DMRG_BLOCK_HPP__
