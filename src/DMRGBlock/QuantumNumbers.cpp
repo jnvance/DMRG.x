a9fa5ff2f4bcb74b023d496ccf4672f2c6b38c4a
James Vance <jvance@ictp.it>
2018-01-16 17:41:53 +0100

    Error consistency and cleanup

--- src/DMRGBlock/QuantumNumbers.cpp
+++ src/DMRGBlock/QuantumNumbers.cpp
@@ -1,5 +1,9 @@
 #include "QuantumNumbers.hpp"
 
+/* Internal macro for checking the initialization state of the qn object */
+#define CheckInit(func) \
+    if(PetscUnlikely(!initialized)) \
+        SETERRQ(mpi_comm, 1, "Object not initialized. Call Initialize() first.");
 
 PetscErrorCode QuantumNumbers::Initialize(
     const MPI_Comm& mpi_comm_in,
@@ -17,15 +21,12 @@ PetscErrorCode QuantumNumbers::Initialize(
     num_sectors = (PetscInt) qn_list_in.size();
     qn_list = qn_list_in;
     qn_size = qn_size_in;
-
     qn_offset.resize(num_sectors+1);
     qn_offset[0] = 0;
     for(PetscInt i = 1; i < num_sectors+1; ++i)
         qn_offset[i] = qn_offset[i-1] + qn_size[i-1];
-
     num_states = qn_offset.back();
     initialized = PETSC_TRUE;
-
     return 0;
 }
 
@@ -36,14 +37,11 @@ PetscErrorCode QuantumNumbers::BlockIdxToGlobalRange(
     PetscInt& GlobIdxEnd
     ) const
 {
-    if(PetscUnlikely(!initialized))
-        SETERRQ(mpi_comm, 1, "Object not initialized. Call Initialize() first.");
+    CheckInit(__FUNCTION__);
     if(PetscUnlikely((BlockIdx < 0) || (BlockIdx >= num_sectors)))
-        SETERRQ2(mpi_comm, 1, "Given BlockIdx (%d) out of bounds [0,%d).", BlockIdx, num_sectors);
-
+        SETERRQ2(PETSC_COMM_SELF, 1, "Given BlockIdx (%d) out of bounds [0,%d).", BlockIdx, num_sectors);
     GlobIdxStart = qn_offset[BlockIdx];
     GlobIdxEnd   = qn_offset[BlockIdx + 1];
-
     return 0;
 }
 
@@ -56,18 +54,14 @@ PetscErrorCode QuantumNumbers::OpBlockToGlobalRange(
     PetscBool& flg
     ) const
 {
-    if(PetscUnlikely(!initialized))
-        SETERRQ(mpi_comm, 1, "Object not initialized. Call Initialize() first.");
+    CheckInit(__FUNCTION__);
     if(PetscUnlikely((BlockIdx < 0) || (BlockIdx >= num_sectors)))
-        SETERRQ2(mpi_comm, 1, "Given BlockIdx (%d) out of bounds [0,%d).", BlockIdx, num_sectors);
-
+        SETERRQ2(PETSC_COMM_SELF, 1, "Given BlockIdx (%d) out of bounds [0,%d).", BlockIdx, num_sectors);
     PetscInt BlockIdx_out = BlockIdx + BlockShift;
-    if(BlockIdx_out < 0 || BlockIdx_out >= num_states)
-    {
+    if(BlockIdx_out < 0 || BlockIdx_out >= num_states){
         flg = PETSC_FALSE;
         return 0;
     }
-
     flg = PETSC_TRUE;
     GlobIdxStart = qn_offset[BlockIdx_out];
     GlobIdxEnd   = qn_offset[BlockIdx_out + 1];
@@ -81,18 +75,13 @@ PetscErrorCode QuantumNumbers::QNToGlobalRange(
     PetscInt& GlobIdxEnd
     ) const
 {
-    if(PetscUnlikely(!initialized))
-        SETERRQ(mpi_comm, 1, "Object not initialized. Call Initialize() first.");
-
+    CheckInit(__FUNCTION__);
     /*  Search QNValue from qn_list and get the index */
     PetscInt BlockIdx = std::find(qn_list.begin(), qn_list.end(), QNValue) - qn_list.begin();
-
     if(PetscUnlikely(BlockIdx==num_sectors))
-        SETERRQ1(mpi_comm, 1, "Given QNValue (%g) not found.", QNValue);
-
+        SETERRQ1(PETSC_COMM_SELF, 1, "Given QNValue (%g) not found.", QNValue);
     GlobIdxStart = qn_offset[BlockIdx];
     GlobIdxEnd   = qn_offset[BlockIdx + 1];
-
     return 0;
 }
 
@@ -103,16 +92,11 @@ PetscErrorCode QuantumNumbers::GlobalIdxToBlockIdx(
     ) const
 {
     PetscInt ierr = 0;
-
-    if(PetscUnlikely(!initialized))
-        SETERRQ(mpi_comm, 1, "Object not initialized. Call Initialize() first.");
-
+    CheckInit(__FUNCTION__);
     if(PetscUnlikely(GlobIdx < 0 || GlobIdx >= num_states))
         SETERRQ2(PETSC_COMM_SELF, 1, "Given GlobIdx (%d) out of bounds [0,%d).", GlobIdx, num_states);
-
     BlockIdx = -1;
     while(GlobIdx >= qn_offset[BlockIdx+1]) ++BlockIdx;
-
     return ierr;
 }
 
@@ -123,9 +107,7 @@ PetscErrorCode QuantumNumbers::GlobalIdxToQN(
     ) const
 {
     PetscInt ierr = 0;
-
     PetscInt BlockIdx;
     ierr = GlobalIdxToBlockIdx(GlobIdx, BlockIdx); CHKERRQ(ierr);
-
     return ierr;
 }
