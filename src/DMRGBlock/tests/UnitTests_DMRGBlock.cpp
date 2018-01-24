54020c44d92b7b073ac40463ee1cebf4435bf20a
James Vance <jvance@ictp.it>
2018-01-24 16:48:56 +0100

    Added tests for init and copy constructor

--- src/DMRGBlock/tests/UnitTests_DMRGBlock.cpp
+++ src/DMRGBlock/tests/UnitTests_DMRGBlock.cpp
@@ -8,6 +8,121 @@ static char help[] =
 
 PETSC_EXTERN PetscErrorCode SetRow(const Mat& A, const PetscInt& row, const std::vector<PetscInt>& idxn);
 
+static char hborder[] = //"************************************************************"
+                        //"************************************************************";
+                        "------------------------------------------------------------"
+                        "------------------------------------------------------------";
+
+#define PrintHeader(COMM,TEXT) \
+    ierr = PetscPrintf((COMM), "%s\n%s\n%s", hborder, (TEXT), hborder); CHKERRQ(ierr);
+
+PetscErrorCode SetSz0(const Mat& Sz)
+{
+    PetscErrorCode ierr = 0;
+
+    ierr = SetRow(Sz, 0, {0,1}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 1, {1}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 2, {2,3,4}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 3, {3}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 4, {2,4}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 5, {5,6}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 7, {7}); CHKERRQ(ierr);
+
+    return ierr;
+}
+
+PetscErrorCode SetSp0(const Mat& Sp)
+{
+    PetscErrorCode ierr = 0;
+
+    ierr = SetRow(Sp, 0, {2,3,4}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 1, {2,4}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 2, {5,6}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 3, {5}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 4, {6}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 6, {7}); CHKERRQ(ierr);
+
+    return ierr;
+}
+
+PetscErrorCode SetSz1(const Mat& Sz)
+{
+    PetscErrorCode ierr = 0;
+
+    ierr = SetRow(Sz, 0, {0,1}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 1, {1}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 3, {3}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 4, {2,4}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 5, {5,6}); CHKERRQ(ierr);
+    ierr = SetRow(Sz, 7, {7}); CHKERRQ(ierr);
+
+    return ierr;
+}
+
+PetscErrorCode SetSp1(const Mat& Sp)
+{
+    PetscErrorCode ierr = 0;
+
+    ierr = SetRow(Sp, 0, {2,3,4}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 1, {2,4}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 2, {5,6}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 4, {6}); CHKERRQ(ierr);
+    ierr = SetRow(Sp, 6, {7}); CHKERRQ(ierr);
+
+    return ierr;
+}
+
+/** Tests initialization and ownership transfer*/
+PetscErrorCode Test_InitAndTransfer()
+{
+    PetscErrorCode ierr = 0;
+
+    Block_SpinOneHalf blk;
+    ierr = blk.Initialize(PETSC_COMM_WORLD, 2, 8);CHKERRQ(ierr);
+    ierr = blk.Magnetization.Initialize(PETSC_COMM_WORLD, {1.5,0.5,-0.5,-1.5}, {2,3,2,1}); CHKERRQ(ierr);
+    ierr = blk.CheckSectors(); CHKERRQ(ierr);
+
+    {
+        /*  Set the entries of Sz[0] following the correct sectors */
+        ierr = SetSz0(blk.Sz[0]); CHKERRQ(ierr);
+        ierr = MatPeek(blk.Sz[0], "blk.Sz[0]"); CHKERRQ(ierr);
+        ierr = blk.MatCheckOperatorBlocks(OpSz, 0); CHKERRQ(ierr);
+
+        /*  Set the entries of Sp[0] following the correct sectors */
+        ierr = SetSp0(blk.Sp[0]); CHKERRQ(ierr);
+        ierr = MatPeek(blk.Sp[0], "blk.Sp[0]"); CHKERRQ(ierr);
+        ierr = blk.MatCheckOperatorBlocks(OpSp, 0); CHKERRQ(ierr);
+
+        /*  Set the entries of Sz[1] following the correct sectors */
+        ierr = SetSz1(blk.Sz[1]); CHKERRQ(ierr);
+        ierr = MatPeek(blk.Sz[1], "blk.Sz[1]"); CHKERRQ(ierr);
+        ierr = blk.MatCheckOperatorBlocks(OpSz, 1); CHKERRQ(ierr);
+
+        /*  Set the entries of Sp[1] following the correct sectors */
+        ierr = SetSp1(blk.Sp[1]); CHKERRQ(ierr);
+        ierr = MatPeek(blk.Sp[1], "blk.Sp[1]"); CHKERRQ(ierr);
+        ierr = blk.MatCheckOperatorBlocks(OpSp, 1); CHKERRQ(ierr);
+    }
+
+    /* Copy to blk2 */
+    Block_SpinOneHalf blk2 = blk;
+
+    ierr = MatPeek(blk2.Sz[0], "blk2.Sz[0]"); CHKERRQ(ierr);
+    ierr = MatPeek(blk2.Sp[0], "blk2.Sp[0]"); CHKERRQ(ierr);
+    ierr = MatPeek(blk2.Sz[1], "blk2.Sz[1]"); CHKERRQ(ierr);
+    ierr = MatPeek(blk2.Sp[1], "blk2.Sp[1]"); CHKERRQ(ierr);
+
+    ierr = blk2.MatCheckOperatorBlocks(OpSz, 0); CHKERRQ(ierr);
+    ierr = blk2.MatCheckOperatorBlocks(OpSp, 0); CHKERRQ(ierr);
+    ierr = blk2.MatCheckOperatorBlocks(OpSz, 1); CHKERRQ(ierr);
+    ierr = blk2.MatCheckOperatorBlocks(OpSp, 1); CHKERRQ(ierr);
+
+    /* Call Destroy only on blk2 which should also destroy matrices in blk */
+    ierr = blk2.Destroy(); CHKERRQ(ierr);
+
+    return ierr;
+}
+
 /** Tests the CheckOperatorBlocks method */
 PetscErrorCode Test_MatCheckOperatorBlocks()
 {
@@ -59,7 +174,9 @@ PetscErrorCode Test_MatCheckOperatorBlocks()
             if(ierr!=PETSC_ERR_ARG_OUTOFRANGE){
                 SETERRQ(PETSC_COMM_SELF, 1, "Failed test");
             } else {
-                printf("\n    Exception caught. :)\n\n");
+                printf("%s\n",hborder);
+                printf("%s   Exception caught.\n",__FUNCTION__);
+                printf("%s\n",hborder);
             }
         }
     }
@@ -78,6 +195,9 @@ int main(int argc, char **argv)
     ierr = MPI_Comm_size(comm, &nprocs); CHKERRQ(ierr);
     ierr = MPI_Comm_rank(comm, &rank); CHKERRQ(ierr);
 
+    PrintHeader(comm, "Test 01: Test_InitAndTransfer");
+    ierr = Test_InitAndTransfer(); CHKERRQ(ierr);
+    PrintHeader(comm, "Test 02: Test_MatCheckOperatorBlocks");
     ierr = Test_MatCheckOperatorBlocks(); CHKERRQ(ierr);
 
     ierr = SlepcFinalize(); CHKERRQ(ierr);
