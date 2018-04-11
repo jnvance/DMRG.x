4fc3ad8b200b7ed68f291d16f6e102438be87954
James Vance <jvance@ictp.it>
2018-04-12 01:15:14 +0200

    Edited private parameter names

--- include/Hamiltonians.hpp
+++ include/Hamiltonians.hpp
@@ -47,30 +47,30 @@ namespace Hamiltonians
         {
             PetscErrorCode ierr;
             /* Get values from command line options */
-            ierr = PetscOptionsGetReal(NULL,NULL,"-J1",&J1,NULL); CHKERRQ(ierr);
-            ierr = PetscOptionsGetReal(NULL,NULL,"-J2",&J2,NULL); CHKERRQ(ierr);
-            ierr = PetscOptionsGetReal(NULL,NULL,"-Jz1",&Jz1,NULL); CHKERRQ(ierr);
-            ierr = PetscOptionsGetReal(NULL,NULL,"-Jz2",&Jz2,NULL); CHKERRQ(ierr);
-            ierr = PetscOptionsGetInt(NULL,NULL,"-Lx",&Lx,NULL); CHKERRQ(ierr);
-            ierr = PetscOptionsGetInt(NULL,NULL,"-Ly",&Ly,NULL); CHKERRQ(ierr);
+            ierr = PetscOptionsGetReal(NULL,NULL,"-J1",&_J1,NULL); CHKERRQ(ierr);
+            ierr = PetscOptionsGetReal(NULL,NULL,"-J2",&_J2,NULL); CHKERRQ(ierr);
+            ierr = PetscOptionsGetReal(NULL,NULL,"-Jz1",&_Jz1,NULL); CHKERRQ(ierr);
+            ierr = PetscOptionsGetReal(NULL,NULL,"-Jz2",&_Jz2,NULL); CHKERRQ(ierr);
+            ierr = PetscOptionsGetInt(NULL,NULL,"-Lx",&_Lx,NULL); CHKERRQ(ierr);
+            ierr = PetscOptionsGetInt(NULL,NULL,"-Ly",&_Ly,NULL); CHKERRQ(ierr);
             ierr = PetscOptionsGetBool(NULL,NULL,"-verbose",&verbose,NULL); CHKERRQ(ierr);
             /* TODO: Also get boundary conditions from command line */
             PetscBool BCopen = PETSC_FALSE;
             ierr = PetscOptionsGetBool(NULL,NULL,"-BCopen",&BCopen,NULL); CHKERRQ(ierr);
-            if(BCopen) { BCx=OpenBC; BCy=OpenBC; }
+            if(BCopen) { _BCx=OpenBC; _BCy=OpenBC; }
             PetscBool BCperiodic = PETSC_FALSE;
             ierr = PetscOptionsGetBool(NULL,NULL,"-BCperiodic",&BCperiodic,NULL); CHKERRQ(ierr);
-            if(BCperiodic) { BCx=PeriodicBC; BCy=PeriodicBC; }
+            if(BCperiodic) { _BCx=PeriodicBC; _BCy=PeriodicBC; }
             set_from_options = PETSC_TRUE;
             return(0);
         }
 
         /** Returns the number of sites in the square lattice */
-        PetscInt NumSites() const { return Lx*Ly; }
+        PetscInt NumSites() const { return _Lx*_Ly; }
 
         /** Returns the number of sites in a single cluster/column of the environment block as suggested by Liang and
             Pang, 1994, for the square lattice */
-        PetscInt NumEnvSites() const { return Ly; }
+        PetscInt NumEnvSites() const { return _Ly; }
 
         /** Returns the object used to construct the Hamiltonian using sites counted from the left side of the lattice */
         std::vector< Term > H(
@@ -80,14 +80,14 @@ namespace Hamiltonians
         /** Prints out some information to stdout */
         void PrintOut() const {
             printf( "HAMILTONIAN: J1J2XYModel_SquareLattice\n");
-            printf( "  Lx  : %lld\n", LLD(Lx));
-            printf( "  Ly  : %lld\n", LLD(Ly));
-            printf( "  J1  : %g\n", J1);
-            printf( "  Jz1 : %g\n", Jz1);
-            printf( "  J2  : %g\n", J2);
-            printf( "  Jz2 : %g\n", Jz2);
-            printf( "  BCx : %s\n", BCx?"Periodic":"Open");
-            printf( "  BCy : %s\n", BCy?"Periodic":"Open");
+            printf( "  Lx  : %lld\n", LLD(_Lx));
+            printf( "  Ly  : %lld\n", LLD(_Ly));
+            printf( "  J1  : %g\n", _J1);
+            printf( "  Jz1 : %g\n", _Jz1);
+            printf( "  J2  : %g\n", _J2);
+            printf( "  Jz2 : %g\n", _Jz2);
+            printf( "  BCx : %s\n", _BCx?"Periodic":"Open");
+            printf( "  BCy : %s\n", _BCy?"Periodic":"Open");
         }
 
         /** Writes out some JSON information to stdout */
@@ -95,18 +95,22 @@ namespace Hamiltonians
             fprintf(fp, "  \"Hamiltonian\": {\n");
             fprintf(fp, "    \"label\":\"J1J2XYModel_SquareLattice\",\n");
             fprintf(fp, "    \"parameters\": {\n");
-            fprintf(fp, "      \"Lx\"  : %lld,\n", LLD(Lx));
-            fprintf(fp, "      \"Ly\"  : %lld,\n", LLD(Ly));
-            fprintf(fp, "      \"J1\"  : %g,\n", J1);
-            fprintf(fp, "      \"Jz1\" : %g,\n", Jz1);
-            fprintf(fp, "      \"J2\"  : %g,\n", J2);
-            fprintf(fp, "      \"Jz2\" : %g,\n", Jz2);
-            fprintf(fp, "      \"BCx\" : \"%s\",\n", BCx?"Periodic":"Open");
-            fprintf(fp, "      \"BCy\" : \"%s\"\n", BCy?"Periodic":"Open");
+            fprintf(fp, "      \"Lx\"  : %lld,\n", LLD(_Lx));
+            fprintf(fp, "      \"Ly\"  : %lld,\n", LLD(_Ly));
+            fprintf(fp, "      \"J1\"  : %g,\n", _J1);
+            fprintf(fp, "      \"Jz1\" : %g,\n", _Jz1);
+            fprintf(fp, "      \"J2\"  : %g,\n", _J2);
+            fprintf(fp, "      \"Jz2\" : %g,\n", _Jz2);
+            fprintf(fp, "      \"BCx\" : \"%s\",\n", _BCx?"Periodic":"Open");
+            fprintf(fp, "      \"BCy\" : \"%s\"\n", _BCy?"Periodic":"Open");
             fprintf(fp, "    }\n");
             fprintf(fp, "  }");
             fflush(fp);
         }
+
+        PetscInt Lx() const { return _Lx; }
+
+        PetscInt Ly() const { return _Ly; }
 
     private:
 
@@ -114,31 +118,31 @@ namespace Hamiltonians
         PetscBool set_from_options = PETSC_FALSE;
 
         /** Transverse anisotropy for nearest-neighbor interactions */
-        PetscScalar Jz1 = 0.0;
+        PetscScalar _Jz1 = 0.0;
 
         /** Transverse anisotropy for next-nearest-neighbor interactions */
-        PetscScalar Jz2 = 0.0;
+        PetscScalar _Jz2 = 0.0;
 
         /** Coupling strength for nearest-neighbor interactions */
-        PetscScalar J1 = 1.0;
+        PetscScalar _J1 = 1.0;
 
         /** Coupling strength for next-nearest-neighbor interactions */
-        PetscScalar J2 = 1.0;
+        PetscScalar _J2 = 1.0;
 
         /** Length along (growing) longitudinal direction */
-        PetscInt Lx = 4;
+        PetscInt _Lx = 4;
 
         /** Length along (fixed) transverse direction */
-        PetscInt Ly = 3;
+        PetscInt _Ly = 3;
 
         /** Flag that tells whether to print some information */
         PetscBool verbose = PETSC_FALSE;
 
         /** Boundary conditions for the longitudinal direction (defaults to cylindrical BC) */
-        BC_t BCx = OpenBC;
+        BC_t _BCx = OpenBC;
 
         /** Boundary conditions for the transverse direction (defaults to cylindrical BC) */
-        BC_t BCy = PeriodicBC;
+        BC_t _BCy = PeriodicBC;
 
         /** Prints some information related to the parameters of the Hamiltonian */
         PetscErrorCode PrintInfo() const
